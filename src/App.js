9770fd8913b0747108ede249927df7fa40edc5c7
TopDev1028 <app.seed5406@gmail.com>
2023-11-17 20:47:10 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useNavigate,
+  Navigate,
+} from "react-router-dom";
+
+import Landing from "./pages/Landing";
+import ListGPT from "./pages/ListGPT";
+import AddGPT from "./pages/AddGPT";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Landing />
+              </>
+            }
+          />
+          <Route
+            path="/list"
+            element={
+              <>
+                <ListGPT />
+              </>
+            }
+          />
+          <Route
+            path="/add"
+            element={
+              <>
+                <AddGPT />
+              </>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
